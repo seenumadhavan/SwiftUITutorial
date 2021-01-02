a803a6dee15a47a40ef388ab5b16cc546dc331af
Srinivasan Madhavan <seenum@berkeley.edu>
2021-01-01 21:42:53 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Seenu Madhavan on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
